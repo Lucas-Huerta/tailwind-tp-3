4637e60ec11c9ec80f0fe56d30d79326f93cee0e
Lucas Huerta <huertalucas13@gmail.com>
2023-11-21 10:31:11 +0100

    first card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-      extend: {},
+      extend: {
+        colors: {
+          lightChips: "#FBE7F2", 
+          darkChips: "#AA0E52", 
+          lightBackground: "#F5F4F4",
+          cardBackground: "#F4F4F5",
+          darkCardBackground: "#292227",
+        }, 
+      },
   },
   plugins: [],
   darkMode: "class",
